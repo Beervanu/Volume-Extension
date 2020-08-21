40cab99c65e15fd90081a66a5595afe8787be876
Jose <fialhoj986@gmail.com>
2020-08-22 00:00:30 +0100

    The UI of the extension changed slightly

--- volumeExtension/js/background.js
+++ volumeExtension/js/background.js
@@ -1,9 +1,13 @@
 chrome.tabs.onRemoved.addListener(deleteTab);
 chrome.runtime.onMessage.addListener(onMessage);
+chrome.tabs.onCreated.addListener(initialiseTab);
 chrome.storage.sync.clear()
 var storage = {};
 const multiplier = 7;
-const defaultPercent = 100/multiplier;
+chrome.storage.sync.set({
+	masterVolume: 100,
+	masterState: false
+})
 // chrome.tabs.onUpdated.addListener(onTabUpdate)
 
 console.log('i spy a gamer')
@@ -25,10 +29,14 @@ function initialiseTab(tab) {
 	if (!ownProp(storage, tab.id)) {
 		console.log("!initialise tab")
 		storage[tab.id] = {}
-		chrome.storage.sync.get(tab.id.toString(), obj => {
+		chrome.storage.sync.get(null, obj => {
 			if (!ownProp(obj, tab.id.toString())) {
-				chrome.storage.sync.set({[tab.id]: {on: true}})
+				chrome.storage.sync.set({[tab.id]: {
+					on: true,
+					tabVolume: 100
+				}})
 			}
+			console.log(obj)
 		})
 	}
 }
@@ -44,22 +52,24 @@ function deleteTab(tabId, removeInfo) {
 
 function onMessage(message, sender) {
 	console.log("!message")
-	initialiseTab(message.tab)
-	if (message.action === "changeVolume") {
+	if (message.action.includes("Tab")) {
+		initialiseTab(message.tab)
+	}
+	if (message.action === "changeTabVolume") {
 		if (ownProp(storage[message.tab.id], "gainNode")) {	
-			console.log("!change volume")
+			console.log("!change tab volume")
 			console.log(message.percent*multiplier/100)
 			storage[message.tab.id].gainNode.gain.value = message.percent*multiplier/100
 		}
-	} else if (message.action === "turnOff") {
-		console.log("!off")
+	} else if (message.action === "turnTabOff") {
+		console.log("!tab off")
 		if (ownProp(storage[message.tab.id], 'stream')) {
 			storage[message.tab.id].stream.getAudioTracks()[0].stop()
 		}
 		deleteTab(message.tab.id, {})
 		chrome.browserAction.setBadgeText({text: "", tabId: message.tab.id})
-	} else if (message.action === "turnOn") {
-		console.log("!on")
+	} else if (message.action === "turnTabOn") {
+		console.log("!tab on")
 		var captured = false;
 		if (ownProp(storage[message.tab.id], 'audioContext')) {
 			captured = true;
@@ -81,12 +91,24 @@ function onMessage(message, sender) {
 
 		
 		onMessage({
-			action: "changeVolume",
+			action: "changeTabVolume",
 			tab: message.tab,
 			percent: message.percent/multiplier
 		}, {})
 		chrome.browserAction.setBadgeText({text: Math.round(message.percent).toString(), tabId: message.tab.id})
-	}
+	} 
+	// else if (message.action === "changeMasterVolume") {
+	// 	console.log("!change master volume")
+	// 	chrome.browserAction.setTitle({title: Math.round(message.percent*multiplier).toString()})
+	// } else if (message.action === "turnMasterOn") {
+	// 	console.log("!master on")
+	// 	chrome.desktopCapture.chooseDesktopMedia(["audio", "window", "screen", "tab"], (streamId, options) => {
+	// 		console.log(streamId)
+	// 		console.log(options)
+	// 	})
+	// } else if (message.action === "turnMasterOff") {
+	// 	console.log("!master off")
+	// }
 }
 
 function setUp(stream, percent, tab) {
--- volumeExtension/js/popup.js
+++ volumeExtension/js/popup.js
@@ -1,35 +1,21 @@
-let grabber = document.getElementById('grabber');
-let barFill = document.getElementById('barFill');
-let volumeTrack = document.getElementById('volumeTrack');
-let volumeSlider = document.getElementById('volumeSlider');
-let volumeTip = document.getElementById('volumeTip');
 const multiplier = 7;
-const defaultPercent = 100/multiplier;
 const perfectWidth = 31.7344;
 
-volumeSlider.addEventListener("mousemove", event => {
-	onMouseMove(event);
-});
+$("#volumeSlider").mousemove(event => {onMouseMove(event)});
 
-volumeSlider.addEventListener("mouseleave", event => {
-	onMouseLeave(event);
-});
+$("#volumeSlider").mouseleave(event => {onMouseLeave(event)});
 
-volumeSlider.addEventListener("mouseenter", event => {
-	onMouseEnter(event);
-});
+$("#volumeSlider").mouseenter(event => {onMouseEnter(event)});
 
-volumeTip.addEventListener("mouseenter", event => {
-	onMouseEnter(event);
-});
+$("#volumeTip").mouseenter(event => {onMouseEnter(event)});
 
-volumeTip.addEventListener("keypress", event => {
+$("#volumeTip").keypress(event => {
 	if (!/[0-9]/.test(event.key)) {
 		event.preventDefault()
 	}
 })
 
-$(volumeTip).click(function (){
+$("#volumeTip").click(function (){
     var range, selection;
     
     if (window.getSelection && document.createRange) {
@@ -45,77 +31,141 @@ $(volumeTip).click(function (){
     }
 });
 
+// $("#mastervolumeTip").click(function (){
+//     var range, selection;
+    
+//     if (window.getSelection && document.createRange) {
+//         selection = window.getSelection();
+//         range = document.createRange();
+//         range.selectNodeContents($(this)[0]);
+//         selection.removeAllRanges();
+//         selection.addRange(range);
+//     } else if (document.selection && document.body.createTextRange) {
+//         range = document.body.createTextRange();
+//         range.moveToElementText($(this)[0]);
+//         range.select();
+//     }
+// });
+
+// $("#mastervolumeSlider").mousemove(event => {onMouseMove(event, master=true)});
+
+// $("#mastervolumeSlider").mouseleave(event => {onMouseLeave(event, master=true)});
+
+// $("#mastervolumeSlider").mouseenter(event => {onMouseEnter(event, master=true)});
+
+// $("#mastervolumeTip").mouseenter(event => {onMouseEnter(event, master=true)});
+
+// $("#mastervolumeTip").keypress(event => {
+// 	if (!/[0-9]/.test(event.key)) {
+// 		event.preventDefault()
+// 	}
+// })
+
 $("#checkbox").click(() => {
 	getCurrentTab(tab => {
 		turnToState($("#checkbox").is(":checked"), tab);
 	});	
 })
 
-volumeTip.addEventListener("keyup", event => {
+// $("#mastercheckbox").click(() => {
+// 	getCurrentTab(tab => {
+// 		turnMasterToState($("#mastercheckbox").is(":checked"));
+// 	});	
+// })
+
+$("#volumeTip").keyup(event => {
 	if (event.keyCode == 13) {
-		submitVolume()
+		submitTabVolume()
 	}
 })
 
-volumeTip.addEventListener("focusout", event => {
-	submitVolume()
-})
+$("#volumeTip").focusout(event => {submitTabVolume()})
 
-function submitVolume() {
-	var percent = Number(volumeTip.textContent.replace("%", ""))
+// $("#mastervolumeTip").focusout(event => {submitMasterVolume()})
+
+// $("#mastervolumeTip").keyup(event => {
+// 	if (event.keyCode == 13) {
+// 		submitMasterVolume()
+// 	}
+// })
+
+function submitTabVolume() {
+	var percent = Number($("#volumeTip").textContent.replace("%", ""))
 	if (percent >= multiplier *100) {
 		percent=700
 	}
-	setElements(percent/multiplier)
+	setVolumeElements(percent/multiplier)
 	getCurrentTab(tab=>{
-		chrome.browserAction.setTitle({title: Math.round(percent).toString(), tabId: tab.id})
+		// chrome.browserAction.setTitle({title: Math.round(percent).toString(), tabId: tab.id})
 		if ($("#checkbox").is(":checked")) {
 			chrome.browserAction.setBadgeText({text: Math.round(percent).toString(), tabId: tab.id})
 		}
+		setInfo(tab.id, "tabVolume", Math.round(percent).toString())
 		chrome.runtime.sendMessage({
-			action: "changeVolume",
+			action: "changeTabVolume",
 			tab: tab,
 			percent: percent/multiplier
 		})
 	})
 }
 
-function onMouseLeave(e) {
-	volumeTip.style.visibility = "hidden";
+// function submitMasterVolume() {
+// 	var percent = Number($("#mastervolumeTip").text().replace("%", ""))
+// 	if (percent >= multiplier *100) {
+// 		percent=700
+// 	}
+// 	console.log
+// 	setVolumeElements(percent/multiplier, master=true)
+// 	chrome.runtime.sendMessage({
+// 		action: "changeMasterVolume",
+// 		percent: percent/multiplier
+// 	})
+// }
+
+function onMouseLeave(e, master=false) {
+	if (master) {master = "master"} else {master = ""}
+	console.log(master)
+	console.log("leve")
+	$(`#${master}volumeTip`).css("visibility", "hidden")
 };
 
-function onMouseEnter(e) {
-	volumeTip.style.visibility = "visible";
+function onMouseEnter(e, master=false) {
+	if (master) {master = "master"} else {master = ""}
+	console.log("enter")
+	console.log(master)
+	$(`#${master}volumeTip`).css("visibility", "visible")
 };
 
 function onBrowserAction(tab) {
 	var on = true;
-	chrome.browserAction.getTitle({tabId: tab.id}, result=>{
-		if (!Number.isNaN(Number(result))) {
-			setElements(Number(result)/multiplier);
-		} else {
-			setElements(defaultPercent)
+	var result = 100;
+	chrome.storage.sync.get(null, storage => {
+		if (ownProp(storage, tab.id)) {
+			on = storage[tab.id].on
+			result = storage[tab.id].tabVolume
 		}
-		chrome.storage.sync.get(tab.id.toString(), obj => {
-			console.log(obj)
-			if (ownProp(obj, tab.id)) {
-				on = obj[tab.id].on
-			}
-			turnToState(on, tab)
-		})
+		setVolumeElements(result/multiplier);
+		// setVolumeElements(storage.masterVolume/multiplier, master=true)
+		console.log(storage)
+		turnToState(on, tab)
+		// turnMasterToState(storage.masterState)
 	})
 };
 
-function setElements(percent, tabId) {
-	volumeTip.style.left = barFill.style.width = grabber.style.left = percent + "%";
-	volumeTip.innerHTML = Math.round(percent*multiplier) + "%";
-	$(".bubbleContainer").css('left', 18 - $(volumeTip).width()/2 + "px")
-	$(".bubbleContainer").css('right', 18 + $(volumeTip).width()/2 + "px")
+function setVolumeElements(percent, master=false) {
+	if (master) {master = "master"} else {master = ""}
+	$(`#${master}volumeTip, #${master}grabber`).css("left", percent + "%")
+	$(`#${master}barFill`).css("width", percent + "%")
+	$(`#${master}volumeTip`).text(Math.round(percent*multiplier) + "%")
+	$(`#${master}bubbleContainer`).css('left', 18 - $(`#${master}volumeTip`).width()/2 + "px")
+	$(`#${master}bubbleContainer`).css('right', 18 + $(`#${master}volumeTip`).width()/2 + "px")
 };
 
-function onMouseMove(e) {
+function onMouseMove(e, master=false) {
+	if (master) {master = "master"} else {master = ""}
+	let track = document.getElementById(master + 'volumeTrack');
 	if (e.buttons === 1) {
-		var rect = volumeTrack.getBoundingClientRect();
+		var rect = track.getBoundingClientRect();
 		var percent = ((e.clientX - rect.left)/rect.width) * 100;
 		if (percent > 100) {
 			percent = 100;
@@ -123,19 +173,28 @@ function onMouseMove(e) {
 			percent = 0;
 		}
 
-		setElements(percent);
-
-		getCurrentTab(tab=>{
-			chrome.browserAction.setTitle({title: Math.round(percent*multiplier).toString(), tabId: tab.id})
-			if ($("#checkbox").is(":checked")) {
-				chrome.browserAction.setBadgeText({text: Math.round(percent*multiplier).toString(), tabId: tab.id})
-			}
+		setVolumeElements(percent, master !== "");
+		
+		if (master) {
+			chrome.storage.sync.set({masterVolume:  Math.round(percent*multiplier)})
 			chrome.runtime.sendMessage({
-				action: "changeVolume",
-				tab: tab,
+				action: "changeMasterVolume",
 				percent: percent
 			})
-		})
+		} else {
+			getCurrentTab(tab=>{
+				// chrome.browserAction.setTitle({title: Math.round(percent*multiplier).toString(), tabId: tab.id})
+				if ($("#checkbox").is(":checked")) {
+					chrome.browserAction.setBadgeText({text: Math.round(percent*multiplier).toString(), tabId: tab.id})
+				}
+				setInfo(tab.id, "tabVolume", Math.round(percent*multiplier))
+				chrome.runtime.sendMessage({
+					action: "changeTabVolume",
+					tab: tab,
+					percent: percent
+				})
+			})
+		}
 	}
 };
 
@@ -147,14 +206,16 @@ function getCurrentTab(func) {
 
 function turnToState(on, tab) {
 	if (on) {
-		chrome.browserAction.getTitle({tabId: tab.id}, result => {
-			if (Number.isNaN(Number(result))) {
-				result = 100;
-			}
+		chrome.storage.sync.get(tab.id.toString(), storage => {
+			console.log(storage)
+			result = storage[tab.id].tabVolume
+			// if (Number.isNaN(Number(result))) {
+			// 	result = 100;
+			// }
 
 			$("#checkboxLabel").text("On")
 			chrome.runtime.sendMessage({
-				action: "turnOn",
+				action: "turnTabOn",
 				tab: tab,
 				percent: result
 			})
@@ -163,12 +224,40 @@ function turnToState(on, tab) {
 	} else {
 		$("#checkboxLabel").text("Off")
 		chrome.runtime.sendMessage({
-			action: "turnOff",
+			action: "turnTabOff",
 			tab: tab
 		})
 	}
 	$("#checkbox").prop("checked", on)
-	chrome.storage.sync.set({[tab.id]: {on: on}})
+	setInfo(tab.id, "on", on)
+	// chrome.storage.sync.set({[tab.id]: {on: on}})
+}
+
+function turnMasterToState(on) {
+	if (on) {
+		chrome.storage.sync.get(null, storage => {
+			console.log(storage)
+			result = storage.masterVolume
+			// if (Number.isNaN(Number(result))) {
+			// 	result = 100;
+			// }
+
+			$("#mastercheckboxLabel").text("On")
+			chrome.runtime.sendMessage({
+				action: "turnMasterOn",
+				percent: result
+			})
+		})
+		
+	} else {
+		$("#mastercheckboxLabel").text("Off")
+		chrome.runtime.sendMessage({
+			action: "turnMasterOff"
+		})
+	}
+	$("#mastercheckbox").prop("checked", on)
+	chrome.storage.sync.set({masterState: on})
+	// chrome.storage.sync.set({[tab.id]: {on: on}})
 }
 
 function ownProp(first, second) {
@@ -177,6 +266,15 @@ function ownProp(first, second) {
 	} else {
 		return false;
 	}
+}
+
+function setInfo(tabId, key, value) {
+	chrome.storage.sync.get(tabId.toString(), items => {
+		if (ownProp(items, tabId)) {
+			items[tabId][key] = value;
+			chrome.storage.sync.set({[tabId]: items[tabId]})
+		}
+	})
 }
 
 getCurrentTab(onBrowserAction);
